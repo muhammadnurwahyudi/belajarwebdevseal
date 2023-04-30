219f04deb56ef00fd6f6e44e37a7b24dd3bc2029
muhnurwahyudi <127280046+muhammadnurwahyudi@users.noreply.github.com>
2023-04-30 14:17:07 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nama_kamu = "Malik";
+    $umur = 25;
+    //petik dua
+    echo "Hello world, Welcome ".$nama_kamu;
+    echo " Umur Kamu ".$umur;
+    echo " Tahun";
+    echo "<br/>";
+    echo "Hello world, Welcome ".$nama_kamu." Umur Kamu ".$umur." Tahun ";
+    echo "<br/>";
+    echo "Berarti $nama_kamu lahir di tahun";
+    echo 2023-$umur
+    //petik satu
+    // echo 'Hello world';
+
+?>
